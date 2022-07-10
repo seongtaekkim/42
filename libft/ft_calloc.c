3232e45594babe104c6865b76d8e91327b9f1388
seongtaekkim <chxortnl@gmail.com>
2022-07-10 16:56:27 +0900

    modified:   ft_calloc.c

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: seongtki <seongtki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:07:10 by seongtki          #+#    #+#             */
-/*   Updated: 2022/07/08 16:24:17 by seongtki         ###   ########.fr       */
+/*   Updated: 2022/07/10 16:55:49 by seongtki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*obj;
 
+	if (size == 0 || count == 0)
+	{
+		size = 1;
+		count = 1;
+	}
 	if (((count * size) / size) != count)
 		return (NULL);
-	obj = malloc(count * size);
+	obj = malloc(0);
 	if (!obj)
 		return (NULL);
 	ft_memset(obj, 0, count * size);

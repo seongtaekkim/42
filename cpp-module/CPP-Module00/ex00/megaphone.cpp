7565f7e3a0838132e4f9654e995451274447d780
seongtaekkim <chxortnl@gmail.com>
2022-08-26 10:25:50 +0900

    cpp00 - ex00

---
+++ cpp-module/CPP-Module00/ex00/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongtki <seongtki@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 09:51:10 by seongtki          #+#    #+#             */
+/*   Updated: 2022/08/26 10:25:30 by seongtki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void	toupperMessage(int count, char **messages)
+{
+	int		index;
+	size_t	jndex;
+
+	index = 1;
+	while (index < count)
+	{
+		jndex = 0;
+		while (jndex < strlen(messages[index]))
+		{
+			std::cout << char(toupper(messages[index][jndex]));
+			jndex++;
+		}
+		index++;
+	}
+	std::cout << std::endl;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout <<  "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+		toupperMessage(argc, argv);
+	
+	return (0);
+}

34692106e4c8174b14ad8e60c34671f96c3f788f
seongtaekkim <chxortnl@gmail.com>
2022-09-06 16:20:51 +0900

    add cpp-module01 ex00

---
+++ cpp-module/CPP-module01/ex00/newZombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongtki <seongtki@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 14:04:19 by seongtki          #+#    #+#             */
+/*   Updated: 2022/09/06 14:04:21 by seongtki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.h"
+
+Zombie* newZombie(const std::string &name)
+{
+    Zombie *z;
+    
+    try
+    {
+        z = new Zombie(name);
+    }
+    catch (const std::bad_alloc &e)
+    {
+        std::cout << e.what() << std::endl;
+        return (NULL);
+    }
+    return (z);
+}
---
+++ cpp-module/CPP-module01/ex00/randomChump.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongtki <seongtki@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 14:04:32 by seongtki          #+#    #+#             */
+/*   Updated: 2022/09/06 14:04:33 by seongtki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.h"
+
+void    randomChump(const std::string &name)
+{
+    Zombie *z = newZombie(name);
+    z->announce();
+    try
+    {
+        delete (z);
+    }
+    catch (const std::bad_alloc &e)
+    {
+        std::cout << e.what() << std::endl;
+        return ;
+    }
+    
+}

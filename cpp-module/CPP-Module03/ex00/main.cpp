8fbe34ae7dcdc7163034e20906e3d4abb0bd5f0f
seongtaekkim <chxortnl@gmail.com>
2022-09-28 23:37:48 +0900

    update

--- cpp-module/CPP-Module03/ex00/main.cpp
+++ cpp-module/CPP-Module03/ex00/main.cpp
@@ -5,33 +5,72 @@ class ClapTrap
 {
 private:
  std::string _name;
- int _hitPoints;
- int _energyPoints;
- int _attackDamage;
+ int _hitPoints; // 체력 공격받으면-attackDamage 수리하면+amount
+ int _energyPoints; // 공격,수리 시 -1
+ int _attackDamage; // 공격력
 public:
- ClapTrap()
+ ClapTrap(const std::string &name)
  {
-    this->_hitPoints = 10;
-    this->_energyPoints = 10;
+   this->_name = name;
+   this->_hitPoints = 10;
+   this->_energyPoints = 10;
+   this->_attackDamage = 0;
+   std::cout << "create " << this->_name << std::endl;
+ }
+ ClapTrap(const ClapTrap & c)
+ {
+   this->_name = c._name;
+   this->_hitPoints = c._hitPoints;
+   this->_energyPoints = c._energyPoints;
+   this->_attackDamage = c._attackDamage;
+ }
+ ~ClapTrap()
+ {
+   std::cout << "die " << this->_name << std::endl;
+ }
+ ClapTrap & operator = (ClapTrap & c)
+ {
+   if (this == &c)
+      return (c);
+   this->_name = c._name;
+   this->_hitPoints = c._hitPoints;
+   this->_energyPoints = c._energyPoints;
+   this->_attackDamage = c._attackDamage;
+   return (*this);
  }
  void attack(const std::string& target)
  {
-    //ClapTrap <name> attacks <target>, causing <damage> points of damage!
-    std::cout << this->_name << " attacks " << target << " causing " << this->_hitPoints << std::endl;
+    std::cout << this->_name << " attacks " << target << " causing " << this->_attackDamage << " points of damage!"<< std::endl;
  }
  void takeDamage(unsigned int amount)
  {
-
+  this->_hitPoints =- amount;
+  std::cout << this->_name << " took " << amount << " damage." << std::endl;
  }
  void beRepaired(unsigned int amount)
  {
-
+  this->_hitPoints += amount;
+  std::cout << this->_name << " recovered " << amount << " hit point." << std::endl;
  }
-
+ std::string getName() const
+ {
+  return (this->_name);
+ }
+ int getAttackDamage() const
+ {
+  return (this->_attackDamage);
+ }
 };
 
 
 int main(void)
 {
-    return (0);
+  ClapTrap java("java");
+  ClapTrap spring("spring");
+
+  java.attack(spring.getName());
+  spring.takeDamage(java.getAttackDamage());
+  java.beRepaired(5);
+
+  return (0);
 }

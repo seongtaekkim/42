e1d641203addec726663853a70922f8ebfafc38e
seongtaekkim <chxortnl@gmail.com>
2022-11-07 21:47:04 +0900

    modify semapore's delecated function : sem_init

--- Philosophers/example/semaphore1.c
+++ Philosophers/example/semaphore1.c
@@ -3,7 +3,7 @@
 #include <semaphore.h>
 int sum = 0;  // a shared variable
 
-sem_t sem;
+sem_t *sem;
 
 void *counter(void *param)
 {
@@ -11,11 +11,11 @@ void *counter(void *param)
     for (k = 0; k < 10000; k++)
     {
         /* entry section */
-        sem_wait(&sem);
+        sem_wait(sem);
         /* critical section */
         sum++;
         /* exit section */
-        sem_post(&sem);
+        sem_post(sem);
         /* remainder section */
     }
     pthread_exit(0);
@@ -24,10 +24,16 @@ void *counter(void *param)
 int main()
 {
     pthread_t tid1, tid2;
-    sem_init(&sem, 0, 1);
+    
+    //sem_init(&sem, 0, 1);
+    sem = sem_open("test_sem", O_CREAT | O_EXCL, 0644, 1); 
     pthread_create(&tid1, NULL, counter, NULL);
     pthread_create(&tid2, NULL, counter, NULL);
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
+    int ret = sem_close(sem); // 세마포어 종료 및 할당한 자원 해제
+	//printf("sem_close: %d\n", ret);
+	ret = sem_unlink("test_sem"); // 세마포어 객체 해제
+	//printf("sem_unlink: %d\n", ret);
     printf("sum = %d\n", sum);
 }

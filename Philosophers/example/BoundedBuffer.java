c547952f9c608627ced649c08d9dee82761fcb36
seongtaekkim <chxortnl@gmail.com>
2022-11-07 22:01:30 +0900

    add boundedbuffer to java

--- Philosophers/example/BoundedBuffer.java
+++ Philosophers/example/BoundedBuffer.java
@@ -11,7 +11,9 @@ class ProdRunner implements Runnable {
                 int money = ((int)(1 + Math.random()*9))*10000;
                 cashBox.give(money);
             }
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+            System.out.println("##### give thread : Interrupt");
+        }
     }
 }
 
@@ -27,13 +29,61 @@ class ConsRunner implements Runnable {
                 Thread.sleep((long)(Math.random()*500));
                 int money = cashBox.take();
             }
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+            System.out.println("##### take thread : Interrupt");
+        }
+    }
+}
+
+class CashBox {
+    private int[] buffer;
+    private int count, in, out;
+
+    public CashBox(int bufferSize) {
+        buffer = new int[bufferSize];
+        count = in = out = 0;
+    }
+
+    synchronized public void give(int money) throws InterruptedException {
+        // critical section
+        while (count == buffer.length) {
+            try {
+                wait();
+            } catch(InterruptedException e) {
+                System.out.println("##### give : Interrupt");
+            }
+        }
+        buffer[in] = money;
+        in = (in + 1) % buffer.length;
+        count++;
+        System.out.printf("여기있다, 용돈: %d원\n", money);
+    }
+
+    synchronized public int take() throws InterruptedException {
+        // critical section
+        while (count == 0) {
+            try {
+                wait();
+            } catch(InterruptedException e) {
+                System.out.println("##### take : Interrupt");
+            }
+        }
+        int money = buffer[out];
+        out = (out + 1) % buffer.length;
+        count--;
+        System.out.printf("고마워, 용돈: %d원\n", money);
+
+        notify();
+        return (money);
     }
 }
 
 public class BoundedBuffer {
     public static void main(String[] args) { 
-        CashBox cashBox = new CashBox(1); Thread[] producers = new Thread[1]; Thread[] consumers = new Thread[1]; // Create threads of producers
+        CashBox cashBox = new CashBox(1);
+        Thread[] producers = new Thread[1];
+        Thread[] consumers = new Thread[1];
+        // Create threads of producers
         for (int i = 0; i < producers.length; i++) {
             producers[i] = new Thread(new ProdRunner(cashBox));
             producers[i].start();
---
+++ Philosophers/example/README.md
@@ -0,0 +1,24 @@
+raceCondition1.c
+raceCondition2.c
+RaceCondition1.java
+RaceCondition2.java
+
+peterson.c
+Peterson1.java
+Peterson2.java
+
+mutex_lock.c
+
+semaphore1.c
+semaphore2.c
+
+Monitor1.java ~ Moditor5.java
+
+bounded_buffer.c
+BoundedBuffer.java
+
+
+
+출처
+https://www.inflearn.com/course/%EC%9A%B4%EC%98%81%EC%B2%B4%EC%A0%9C-%EA%B3%B5%EB%A3%A1%EC%B1%85-%EC%A0%84%EA%B3%B5%EA%B0%95%EC%9D%98/dashboard
+운영체제 공룡책 강의 - 주니온

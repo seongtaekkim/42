3e3a8f1efbfb46a81be06230875efb8f19352943
seongtaekkim <chxortnl@gmail.com>
2022-12-27 13:15:21 +0900

    delete unused codes

--- FdF/calc_proj.c
+++ FdF/calc_proj.c
@@ -6,7 +6,7 @@
 /*   By: seongtki <seongtki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 17:40:42 by seongtki          #+#    #+#             */
-/*   Updated: 2022/09/07 17:42:08 by seongtki         ###   ########.fr       */
+/*   Updated: 2022/12/27 13:14:44 by seongtki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,23 @@ static void	conv_to_iso(int *x, int *y, int z)
 
 static void	isometric(t_fdf *fdf, int *x, int *y, int z)
 {
-	int	prev_x;
-	int	prev_y;
 	int	theta;
 
 	theta = fdf->option.rotate;
 	rotate_x(y, &z, degree_to_radian(theta));
 	rotate_y(x, &z, degree_to_radian(theta));
 	rotate_z(x, y, degree_to_radian(theta));
-	prev_x = *x;
-	prev_y = *y;
 	conv_to_iso(x, y, z);
 }
 
 static void	orthographic(t_fdf *fdf, int *x, int *y, int z)
 {
-	int	prev_x;
-	int	prev_y;
 	int	theta;
 
 	theta = fdf->option.rotate;
 	rotate_x(y, &z, degree_to_radian(theta));
 	rotate_y(x, &z, degree_to_radian(theta));
 	rotate_z(x, y, degree_to_radian(theta));
-	prev_x = *x;
-	prev_y = *y;
 }
 
 void	do_project(t_fdf *fdf, int *x, int *y, int z)

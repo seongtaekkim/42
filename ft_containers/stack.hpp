bf3c6c6cc53f697f8c343d278cb5dce0d5753d18
seongtaekkim <chxortnl@gmail.com>
2023-02-13 20:03:10 +0900

    add stack with template 2th parameter : std vector

---
+++ ft_containers/stack.hpp
@@ -0,0 +1,77 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+	#include <iostream>
+	#include <vector>
+	//#include "vector.hpp"
+namespace ft
+{
+
+template <class _Tp, class _Container= std::vector<_Tp> >
+class stack
+{
+public:
+  typedef typename _Container::value_type      value_type;
+  typedef typename _Container::size_type       size_type;
+  typedef          _Container                  container_type;
+
+  typedef typename _Container::reference       reference;
+  typedef typename _Container::const_reference const_reference;
+
+  template <class _Tp1, class _Seq1>
+  friend bool operator== (const stack<_Tp1, _Seq1>&, const stack<_Tp1, _Seq1>&);
+  template <class _Tp1, class _Seq1>
+  friend bool operator< (const stack<_Tp1, _Seq1>&, const stack<_Tp1, _Seq1>&);
+protected:
+  _Container c;
+public:
+  stack() : c() {}
+  explicit stack(const _Container& __s) : c(__s) {}
+
+  bool empty() const { return c.empty(); }
+  size_type size() const { return c.size(); }
+  reference top() { return c.back(); }
+  const_reference top() const { return c.back(); }
+  void push(const value_type& __x) { c.push_back(__x); }
+  void pop() { c.pop_back(); }
+};
+
+template <class _Tp, class _Seq>
+bool operator==(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y)
+{
+  return __x.c == __y.c;
+}
+
+template <class _Tp, class _Seq>
+bool operator<(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y)
+{
+  return __x.c < __y.c;
+}
+
+template <class _Tp, class _Seq>
+bool operator!=(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y)
+{
+  return !(__x == __y);
+}
+
+template <class _Tp, class _Seq>
+bool operator>(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y)
+{
+  return __y < __x;
+}
+
+template <class _Tp, class _Seq>
+bool operator<=(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y)
+{
+  return !(__y < __x);
+}
+
+template <class _Tp, class _Seq>
+bool operator>=(const stack<_Tp,_Seq>& __x, const stack<_Tp,_Seq>& __y)
+{
+  return !(__x < __y);
+}
+
+}
+
+#endif
